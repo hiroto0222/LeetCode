d840b2d10720aa603bb856b9521450c527539be7
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-09-20 21:23:38 +0900

    init

---
+++ 647.palindromic-substrings.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=647 lang=python3
+#
+# [647] Palindromic Substrings
+#
+
+# @lc code=start
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        return
+        
+# @lc code=end
+
