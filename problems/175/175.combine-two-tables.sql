8c9445494e95314a65fdd73e61a7cfd286c80a71
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-10-11 19:34:31 +0900

    175 (easy)

---
+++ problems/175/175.combine-two-tables.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=175 lang=mysql
+--
+-- [175] Combine Two Tables
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT firstName, lastName, city, state
+FROM Person P
+LEFT JOIN Address A
+    ON P.personId = A.personId
+;
+
+-- @lc code=end
+
