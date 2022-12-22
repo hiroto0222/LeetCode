6d0fd0c99fca5c8c78abb6ad4e5a3e4d8211ba97
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2022-12-22 10:56:46 -0600

    206 (easy)

---
+++ problems/206/206.reverse-linked-list.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode curr = head.next;
+        ListNode prev = head;
+        prev.next = null;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
+// @lc code=end
