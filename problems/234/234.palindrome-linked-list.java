380c5f6ca4bd14a2b736ea4c6ccab28c03a0f830
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2022-12-23 18:58:43 -0600

    234 (easy)

---
+++ problems/234/234.palindrome-linked-list.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=234 lang=java
+ *
+ * [234] Palindrome Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head.next == null) {
+            return true;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode curr = mid.next;
+        ListNode prev = mid;
+        mid.next = null;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        while (prev != null && head != null) {
+            if (prev.val != head.val) {
+                return false;
+            }
+            prev = prev.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
+    ListNode getMid(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return fast == null ? slow : slow.next;
+    }
+}
+// @lc code=end
