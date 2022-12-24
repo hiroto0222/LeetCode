3b7f04ce0c00cc8dad2f179c614ddd588d859bef
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2022-12-23 18:21:51 -0600

    142 (medium)

---
+++ problems/142/142.linked-list-cycle-ii.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode intersect = getIntersect(head);
+
+        if (intersect == null) {
+            return null;
+        }
+
+        while (head != intersect) {
+            head = head.next;
+            intersect = intersect.next;
+        }
+
+        return head;
+    }
+
+    ListNode getIntersect(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
+// @lc code=end
