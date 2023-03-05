acd5506ab240bf40e237d8bb9d6354d6ea4256b2
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-03-05 18:44:03 +0900

    139 (medium)

---
+++ problems/139/139.word-break.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=139 lang=python
+#
+# [139] Word Break
+#
+
+# @lc code=start
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        words = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+                    break
+        
+        return dp[n]
+
+        
+# @lc code=end
+
