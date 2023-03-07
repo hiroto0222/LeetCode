9a92e526fea12a0dd5b4c3a1c74f742732af367b
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-03-07 14:20:48 +0900

    347 (medium)

--- problems/347/347.top-k-frequent-elements.py
+++ problems/347/347.top-k-frequent-elements.py
@@ -12,18 +12,37 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # return k most frequent elements from nums
         # nums = [1, 1, 1, 2, 2, 3], k = 2 -> [1, 2]
+
         # using a maxheap then popping k times
         # O(N * log K)
         
-        cnts = Counter(nums)
-        heap = []
-        for num, cnt in cnts.items():
-            heappush(heap, (cnt, num))
-            if len(heap) > k:
-                heappop(heap) # keep popping min vals in heap till k most frequent are left
+        # cnts = Counter(nums)
+        # heap = []
+        # for num, cnt in cnts.items():
+        #     heappush(heap, (cnt, num))
+        #     if len(heap) > k:
+        #         heappop(heap) # keep popping min vals in heap till k most frequent are left
         
-        return [val for k, val in heap]
+        # return [val for k, val in heap]
+
+        # have array cnts
+        # i -> cnt, cnts[i] -> [list of elements]
+        # have hashmap storing (element -> i)
+        # O(N) time, O(N) space
+
+        n = len(nums)
+        indexs = Counter(nums)
+        cnts = [[] for _ in range(n + 1)]
+        for val, cnt in indexs.items():
+            cnts[cnt].append(val)
+        
+        ans = []
+        for i in range(n, -1, -1):
+            while len(cnts[i]) > 0 and k > 0:
+                ans.append(cnts[i].pop())
+                k -= 1
+        
+        return ans
 
 
 # @lc code=end
-
