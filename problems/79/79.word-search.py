f334044ad9a2b843ca371a40477cf4e5119962e0
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-03-22 15:04:02 +0900

    79 (medium)

--- problems/79/79.word-search.py
+++ problems/79/79.word-search.py
@@ -12,29 +12,27 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        # DFS
+        # DFS from starting letter
+
+        ROWS, COLS = len(board), len(board[0])
 
         def dfs(i, j, k):
             if k == len(word):
                 return True
-            
             if i < 0 or i >= ROWS or j < 0 or j >= COLS or board[i][j] != word[k]:
                 return False
             
             temp, board[i][j] = board[i][j], "."
-            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j , k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
             board[i][j] = temp
             return res
-
-
-        ROWS, COLS = len(board), len(board[0])
+        
         for i in range(ROWS):
             for j in range(COLS):
                 if board[i][j] == word[0] and dfs(i, j, 0):
                     return True
-        
+                
         return False
 
 
 # @lc code=end
-
