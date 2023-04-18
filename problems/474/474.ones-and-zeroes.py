469d5514c81dde60a23513c8dc7790daf18ff402
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-18 16:25:39 +0900

    474 (medium)

--- problems/474/474.ones-and-zeroes.py
+++ problems/474/474.ones-and-zeroes.py
@@ -6,32 +6,53 @@
 
 
 # @lc code=start
+import math
+from functools import cache
+
+
 class Solution:
+    # def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+    #     """
+    #     find subset such that
+    #         at most m x "0"
+    #         at most n x "1"
+    #     return size of the largest subset
+
+    #     eg:
+    #     strs = ["10","0001","111001","1","0"], m = 5, n = 3
+
+    #     dp[M][N]
+    #     dp[i][j] -> largest number of items in subset for m = i, n = j
+    #     """
+
+    #     dp = [[0] * (n + 1) for _ in range(m + 1)]
+    #     for s in strs:
+    #         zero_cnt = s.count("0")
+    #         one_cnt = len(s) - zero_cnt
+
+    #         for i in range(m, zero_cnt - 1, -1):
+    #             for j in range(n, one_cnt - 1, -1):
+    #                 dp[i][j] = max(dp[i][j], dp[i - zero_cnt][j - one_cnt] + 1)
+    #         print(dp)
+
+    #     return dp[m][n]
+
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        """
-        find subset such that
-            at most m x "0"
-            at most n x "1"
-        return size of the largest subset
+        cnts = [[s.count("0"), s.count("1")] for s in strs]
 
-        eg:
-        strs = ["10","0001","111001","1","0"], m = 5, n = 3
+        @cache
+        def dp(i, j, idx):
+            if i < 0 or j < 0:
+                return -math.inf
 
-        dp[M][N]
-        dp[i][j] -> largest number of items in subset for m = i, n = j
-        """
+            if idx == len(strs):
+                return 0
 
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for s in strs:
-            zero_cnt = s.count("0")
-            one_cnt = len(s) - zero_cnt
+            return max(
+                dp(i, j, idx + 1), 1 + dp(i - cnts[idx][0], j - cnts[idx][1], idx + 1)
+            )
 
-            for i in range(m, zero_cnt - 1, -1):
-                for j in range(n, one_cnt - 1, -1):
-                    dp[i][j] = max(dp[i][j], dp[i - zero_cnt][j - one_cnt] + 1)
-            print(dp)
-
-        return dp[m][n]
+        return dp(m, n, 0)
 
 
 # @lc code=end
