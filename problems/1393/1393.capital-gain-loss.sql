7ca9de0e0bed0dd697c08397fd906f6f346dd820
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-04-19 22:02:02 +0900

    1393 (medium)

---
+++ problems/1393/1393.capital-gain-loss.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=1393 lang=mysql
+--
+-- [1393] Capital Gain/Loss
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT
+    stock_name,
+    SUM(IF(operation = "Buy", -price, price)) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
+
+-- @lc code=end
+
