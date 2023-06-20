d39d080d0063f44a254084afa832d80c045b3b2a
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-06-20 17:44:38 +0900

    1 (easy)

--- problems/1/1.two-sum.rb
+++ problems/1/1.two-sum.rb
@@ -11,12 +11,9 @@
 def two_sum(nums, target)
   cache = {}
   nums.each_with_index do |num, i|
-    if cache.include?(num)
-      return [cache[num], i]
-    else
-      cache[target - num] = i
-    end
+    return [cache[num], i] if cache.include?(num)
+
+    cache[target - num] = i
   end
 end
 # @lc code=end
-
