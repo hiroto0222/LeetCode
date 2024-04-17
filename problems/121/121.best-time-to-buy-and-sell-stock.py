0f8b9f58cd910c99b804dc961abccf5b0b9cdb66
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-17 21:00:07 +0900

    121 easy

--- problems/121/121.best-time-to-buy-and-sell-stock.py
+++ problems/121/121.best-time-to-buy-and-sell-stock.py
@@ -4,24 +4,30 @@
 # [121] Best Time to Buy and Sell Stock
 #
 
+
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # choose single day to buy and sell stock and return max profit
-        # prices = [7, 1, 5, 3, 6, 4] -> buy 1 sell 6 -> 5
-        # if cost is lower, buy
-        # if cost is higher, keep selling -> obtain max from that
-        # O(N) time, O(1) space
-        
+        # prices = [7,1,5,3,6,4]
+        # always buy lower price
+        # if price is higher, sell and compare with current max
+
+        n = len(prices)
+
+        if n == 0:
+            return 0
+
         curr_max = 0
-        curr = prices[0]
-        for i in range(1, len(prices)):
-            if prices[i] < curr:
-                curr = prices[i]
+        curr_price = prices[0]
+        for i in range(1, n):
+            if prices[i] < curr_price:
+                # buy for lower price, dont sell
+                curr_price = prices[i]
             else:
-                curr_max = max(curr_max, prices[i] - curr)
+                # compare sell price with curr_max
+                curr_max = max(curr_max, prices[i] - curr_price)
 
         return curr_max
 
-        
+
 # @lc code=end
