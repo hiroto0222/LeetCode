99ad7da939c8d7f8b453c9fae50d755bde6b1d39
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-18 21:39:48 +0900

    125. 2 pointers

--- problems/125/125.valid-palindrome.py
+++ problems/125/125.valid-palindrome.py
@@ -4,30 +4,24 @@
 # [125] Valid Palindrome
 #
 
+
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 1. reverse sort and compare -> O(NlogN)
-        # 2. two pointer -> O(N)
-        # O(N) time, O(1) space
-        
-        l, r = 0, len(s) - 1
+        # 2 pointer approach
+        # time O(N), space O(1)
+
+        s = "".join(filter(str.isalnum, s.lower()))
+        n = len(s)
+        l, r = 0, n - 1
+
         while l < r:
-            while l < r and not self.isAlphaNum(s[l]):
-                l += 1
-            while r > l and not self.isAlphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+            if s[l] != s[r]:
                 return False
             l += 1
             r -= 1
-        
-        return True
 
-    def isAlphaNum(self, c):
-        return (ord("a") <= ord(c) <= ord("z")) or \
-               (ord("A") <= ord(c) <= ord("Z")) or \
-               (ord("0") <= ord(c) <= ord("9"))
+        return True
 
 
 # @lc code=end
