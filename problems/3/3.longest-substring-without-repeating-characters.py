e36ff85aac939cd39bf8da91adfedf40806bcb57
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-04-19 21:53:03 +0900

    3. sliding window

--- problems/3/3.longest-substring-without-repeating-characters.py
+++ problems/3/3.longest-substring-without-repeating-characters.py
@@ -4,30 +4,34 @@
 # [3] Longest Substring Without Repeating Characters
 #
 
+
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # find the length of longest substring without repeating chars
-        # sliding window, for each window have hashset of seen
-        # s = "dvdf"
+        # s = "abcabcbb"
+        # sliding window
+        # set to keep track of current characters in window
+        # time O(N), space O(1)
 
-        if len(s) == 0:
+        n = len(s)
+
+        if n == 0:
             return 0
 
+        curr_max = 1
+        curr_chars = set()
         l = 0
-        ans = 0
-        seen = set()
 
-        for r, char in enumerate(s):
-            if char not in seen:
-                seen.add(char)
-            else:
-                while s[l] != char:
-                    seen.remove(s[l])
-                    l += 1
+        for r in range(n):
+            curr = s[r]
+            while curr in curr_chars:
+                curr_chars.remove(s[l])
                 l += 1
-            ans = max(ans, r - l + 1)
-        
-        return ans
-        
+
+            curr_chars.add(curr)
+            curr_max = max(curr_max, r - l + 1)
+
+        return curr_max
+
+
 # @lc code=end
