5f34343cd1eda4400d8d028a3fab6b1b63ec6b95
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-06-14 18:52:50 +0900

    199. java BFS

--- problems/199/199.binary-tree-right-side-view.java
+++ problems/199/199.binary-tree-right-side-view.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /*
  * @lc app=leetcode id=199 lang=java
  *
@@ -8,37 +5,84 @@ import java.util.List;
  */
 
 // @lc code=start
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        helper(root, ans, 0);
-        return ans;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
     }
 
-    private void helper(TreeNode node, List<Integer> ans, int level) {
-        if (node != null) {
-            if (level == ans.size()) {
-                ans.add(node.val);
-            }
-            helper(node.right, ans, level + 1);
-            helper(node.left, ans, level + 1);
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        /*
+         * return right side view of binary tree
+         *
+         * intuition:
+         * - using BFS, last node of each level
+         *
+         * O(N) time, O(N) space
+         */
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
         }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int currVal = q.peek().val;
+            while (size-- > 0) {
+                TreeNode currNode = q.poll();
+                currVal = currNode.val;
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            ans.add(currVal);
+        }
+
+        return ans;
     }
 }
 // @lc code=end
-
