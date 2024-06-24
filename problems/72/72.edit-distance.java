efb99328b3346f373355f1b09a0987614dec3b00
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-06-24 14:12:25 +0900

    72. java dp

--- problems/72/72.edit-distance.java
+++ problems/72/72.edit-distance.java
@@ -7,29 +7,57 @@
 // @lc code=start
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length();
-        int n = word2.length();
+        /*
+         * minimum operations to convert word1 to word2
+         * 1. insert a character
+         * 2. delete a character
+         * 3. replace a character
+         *
+         * compare each letter in word1 and word2
+         * 1. if word1[i] == word2[j] then i++, j++
+         * 2. else consider the 3 operations
+         * a) if insert, then we just increment j, but still need to deal with word1[i]
+         * (i, j + 1)
+         * b) if delete, then we just increment i, but still need to deal with word2[j]
+         * (i + 1, j)
+         * c) if replace, then we increment both
+         * (i + 1, j + 1)
+         *
+         * dp[n][m] where dp[0][0] holds the min operations
+         * dp[i][j] = min(dp[i][j + 1], dp[i + 1][j], dp[i + 1][j + 1])
+         *
+         * base cases are when either string has reached empty
+         * the operations required are the length of the remaining string
+         *
+         * O(N*M) time, O(N*M) space
+         */
 
-        int[][] dp = new int[m + 1][n + 1];
+        int n = word1.length(), m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 0;  // no strings given
-                } else if (i == 0 && j != 0) {
-                    dp[i][j] = j;
-                } else if (i != 0 && j == 0) {
-                    dp[i][j] = i;
-                } else if (word2.charAt(j - 1) != word1.charAt(i - 1)) {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1];
+        // populate base cases
+        for (int i = 0; i <= n; i++) {
+            dp[i][m] = n - i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[n][j] = m - j;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                // if letters are equal, no operations required
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                }
+                // else, take the min of all 3 operations and add 1
+                else {
+                    int val = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                    dp[i][j] = Math.min(val, dp[i + 1][j + 1]) + 1;
                 }
             }
         }
 
-        return dp[m][n];
+        return dp[0][0];
     }
 }
 // @lc code=end
-
