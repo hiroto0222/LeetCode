ec5b739a30af8f6b27ddf168d7cd32f3ca575935
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-07-01 14:28:28 +0900

    64. minimum path sum (dp)

--- problems/64/64.minimum-path-sum.java
+++ problems/64/64.minimum-path-sum.java
@@ -5,31 +5,36 @@
  */
 
 // @lc code=start
+
 class Solution {
     public int minPathSum(int[][] grid) {
-        // move down or right from (0, 0) to (m, n)
-        // find min path sum
-        int m = grid.length, n = grid[0].length;
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
+        /*
+         * given m x n grid, find minimum path
+         *
+         * intuition:
+         * min path at (i,j) is
+         * dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1])
+         * use input grid as dp matrix
+         *
+         * O(N*M) time, O(1) space
+         */
+
+        int m = grid.length;
+        int n = grid[0].length;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                }
-
-                if (i == 0 && j > 0) {
-                    dp[i][j] = dp[i][j - 1] + grid[i][j];
-                } else if (j == 0 && i > 0) {
-                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                if (i == 0) {
+                    grid[i][j] += j == 0 ? 0 : grid[i][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                    grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
                 }
             }
         }
 
-        return dp[m - 1][n - 1];
+        return grid[m - 1][n - 1];
     }
 }
 // @lc code=end
