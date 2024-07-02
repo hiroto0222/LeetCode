ff0fdd6974e4c082f5c30fc866f2d73f1457c4c0
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-07-02 16:18:52 +0900

    931. minimum falling path sum (dp)

--- problems/931/931.minimum-falling-path-sum.java
+++ problems/931/931.minimum-falling-path-sum.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /*
  * @lc app=leetcode id=931 lang=java
  *
@@ -9,20 +7,43 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        // bottom up (tabulation)
+        /*
+         * given nxn matrix, return min sum of any falling path.
+         * start from the first row and col.
+         * (row, col) -> (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)
+         * -100 <= matrix[row][col] <= 100
+         *
+         * intuition:
+         * for each row find the optimal minimum paths from each branch.
+         * bottom-up dp
+         *
+         * O(N*N) time, O(N*N) space
+         */
 
-        Integer inf = Integer.MAX_VALUE;
         int n = matrix.length;
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                int left = j <= 0 ? inf : matrix[i + 1][j - 1];
-                int mid = matrix[i + 1][j];
-                int right = j >= n - 1 ? inf : matrix[i + 1][j + 1];
-                matrix[i][j] += Math.min(Math.min(left, mid), right);
+        int[][] dp = new int[n][n];
+
+        // fill base case
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = matrix[n - 1][i];
+        }
+
+        for (int row = n - 2; row >= 0; row--) {
+            for (int i = 0; i < n; i++) {
+                int left = i == 0 ? dp[row + 1][i] : dp[row + 1][i - 1];
+                int right = i == n - 1 ? dp[row + 1][i] : dp[row + 1][i + 1];
+                dp[row][i] = matrix[row][i] + Math.min(dp[row + 1][i], Math.min(left, right));
             }
         }
 
-        return Arrays.stream(matrix[0]).min().getAsInt();
+        int res = dp[0][0];
+        for (int i = 1; i < n; i++) {
+            if (dp[0][i] < res) {
+                res = dp[0][i];
+            }
+        }
+
+        return res;
     }
 }
 // @lc code=end
