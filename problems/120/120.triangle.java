6cf3dabd258d5dc3ed23f2c4bcacac736fc4048a
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-07-02 15:59:23 +0900

    120. triangle (dp)

--- problems/120/120.triangle.java
+++ problems/120/120.triangle.java
@@ -8,43 +8,33 @@ import java.util.List;
 
 // @lc code=start
 class Solution {
-    private Integer[][] cache;
-
     public int minimumTotal(List<List<Integer>> triangle) {
-        // return minimum path sum from top to bottom
-        int n = triangle.size();
-        cache = new Integer[n][n];
-        return dfs(0, 0, triangle);
-    }
-
-    private int dfs(int i, int j, List<List<Integer>> triangle) {
-        if (cache[i][j] != null) {
-            return cache[i][j];
-        }
-
-        int path = triangle.get(i).get(j);
-        if (i < triangle.size() - 1) {
-            path += Math.min(
-                    dfs(i + 1, j, triangle),
-                    dfs(i + 1, j + 1, triangle));
-        }
-
-        cache[i][j] = path;
-        return path;
-    }
-
-    private int bottomUp(List<List<Integer>> triangle) {
-        // start from bottom to top
+        /*
+         * return min path sum from top to bottom.
+         * can move from (i,j) to (i+1,j) or (i+1,j+1)
+         *
+         * bottom-up dp
+         *
+         * O(N) time, O(N) space where N is the height of the triangle
+         */
 
         int n = triangle.size();
-        int[] dp = new int[n + 1]; // keep track of prev row
+        int[] dp = new int[n];
 
-        for (int i = n - 1; i >= 0; i--) {
-            int[] curr = new int[n + 1];
-            for (int j = 0; j <= i; j++) {
-                curr[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+        if (n == 1) {
+            return triangle.get(0).get(0);
+        }
+
+        // populate base case
+        for (int i = 0; i < triangle.get(n - 1).size(); i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+
+        for (int row = n - 2; row >= 0; row--) {
+            for (int i = 0; i < triangle.get(row).size(); i++) {
+                int curr = triangle.get(row).get(i);
+                dp[i] = curr + Math.min(dp[i], dp[i + 1]);
             }
-            dp = curr;
         }
 
         return dp[0];
