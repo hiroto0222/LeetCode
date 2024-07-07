1de64b5e95d82c66bd0000a7edcb042b46d4ac59
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2024-07-07 13:27:55 +0900

    1143. longest common subsequence (dp)

--- problems/1143/1143.longest-common-subsequence.java
+++ problems/1143/1143.longest-common-subsequence.java
@@ -8,36 +8,26 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         /*
-         * return length of longest common subsequence of text1 and text2
-         * if none return 0
-         *
-         * "abcde"
-         * "abc"
-         * if we find a common letter text1[i] == text2[j]
-         * then 1 + next longest substring...
+         * return length of text1 and text2 longest common subsequence.
+         * if no, return 0.
          *
          * intuition:
-         * - 2D dp
-         * - bottom-up DP
-         * - if text1[i] == text2[j] -> dp[i][j] = 1 + dp[i + 1][j + 1]
-         * - else dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+         * - bottom-up dp
+         * - if text1[i] = text2[j] -> dp(i,j) = 1 + dp(i+1,j+1)
+         * - else -> dp(i,j) = max(dp(i+1,j), dp(i,j+1)) skip i or skip j
          *
-         * O(N*M) time, O(N*M) space
+         * O(N * M) time, O(N * M) space
          */
 
         int n = text1.length();
         int m = text2.length();
-
         int[][] dp = new int[n + 1][m + 1];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                // if equal, then add 1 to prev
                 if (text1.charAt(i) == text2.charAt(j)) {
                     dp[i][j] = 1 + dp[i + 1][j + 1];
-                }
-                // if not equal, find max of either right or down
-                else {
+                } else {
                     dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
